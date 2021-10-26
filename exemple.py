1bfb2e8faec647102161e029b2560546166daee3
zao-oaz <79938367+zao-oaz@users.noreply.github.com>
2021-10-26 15:12:32 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -1,4 +1,15 @@
-
+#Streamlit
 import streamlit as st
-
 st.title("nutrition_app")
+
+#Librairies
+import pandas as pd
+import plotly.express as px
+
+#Dataset
+df = pd.read_csv("C:/Users/zaome/Documents/Alisson/Nutrition/database_clean.csv", sep="|")
+df.head()
+
+#Correlation
+corr = df.corr()
+px.imshow(corr)
