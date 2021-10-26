d8ef4f880a16fbac89bc8c9cd3f691f84756f895
Eden_Lecarpentier <87129559+EdenLecarpentier@users.noreply.github.com>
2021-10-26 16:54:06 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None
+if uploaded_file is not None :
   df = pd.read_csv(uploaded_file, sep="|")
   st.write(df)
   st.line_chart(df)
