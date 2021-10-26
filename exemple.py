e1853896cbbe170adbcb260d5284802b34b79a66
Eden_Lecarpentier <87129559+EdenLecarpentier@users.noreply.github.com>
2021-10-26 16:21:47 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -4,8 +4,6 @@ import pandas as pd
 
 st.title("nutrition_app")
 
-def load_data():
-  csv_file = pd.read_csv("nutrition_databse.csv")
+csv_file = pd.read_csv("nutrition_databse.csv")
   
-def first_plot():
-  st.line_chart(data=load_data, width=100, height=100)
+st.line_chart(data=load_data, width=100, height=100)
