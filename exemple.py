2dbf7559df0ba5ef785acb23cf0d1ce40f6b2e9c
Eden_Lecarpentier <87129559+EdenLecarpentier@users.noreply.github.com>
2021-10-26 16:12:56 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -1,6 +1,10 @@
 import streamlit as st
-
+import pandas as pd
 st.title("nutrition_app")
 
 def load_data():
   csv_file = pd.read_csv("nutrition_databse.csv")
+  
+ def first_plot():
+  st.write(load_data)
+  
