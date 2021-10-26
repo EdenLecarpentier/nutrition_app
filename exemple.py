ff8afc1469c902bd6122633590025ec828032aab
Eden_Lecarpentier <87129559+EdenLecarpentier@users.noreply.github.com>
2021-10-26 16:51:43 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 
 
-st.title("nutrition_app")
-
-csv_file = pd.read_csv("nutrition_databse.csv")
-  
-st.line_chart(df)
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None
+  df = pd.read_csv(uploaded_file, sep="|")
+  st.write(df)
+  st.line_chart(df)
