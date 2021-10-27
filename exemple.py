f7dffe41704c11cc33b4eef1dfe03c85c2937493
Eden_Lecarpentier <87129559+EdenLecarpentier@users.noreply.github.com>
2021-10-27 09:48:06 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -8,12 +8,4 @@ if uploaded_file is not None :
   st.write(df)
   st.line_chart(df)
   
-z = z_data.values
-sh_0, sh_1 = z.shape
-x, y = np.linspace(0, 1, sh_0), np.linspace(0, 1, sh_1)
-fig = go.Figure(data=[go.Surface(z=z, x=x, y=y)])
-fig.update_layout(title="IRR", autosize=False,
-width=800, height=800,
-margin=dict(l=40, r=40, b=40, t=40))
-st.plotly_chart(fig)
  
