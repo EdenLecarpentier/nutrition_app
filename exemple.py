0be48b06472b26e8fad973032a4b81e888bf90e0
Eden_Lecarpentier <87129559+EdenLecarpentier@users.noreply.github.com>
2021-10-27 10:42:44 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -3,7 +3,3 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px 
 
-chunksize = 10 ** 6
-with pd.read_csv(filename, chunksize=chunksize) as reader:
-    for chunk in reader:
-        process(chunk)
