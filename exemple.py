5d456394d57cd554a410d38362c79d475e4001f7
Eden_Lecarpentier <87129559+EdenLecarpentier@users.noreply.github.com>
2021-10-27 09:58:42 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib
 
 
 uploaded_file = st.file_uploader("Choose a file")
@@ -7,5 +8,6 @@ if uploaded_file is not None :
   df = pd.read_csv(uploaded_file, sep="|")
   st.write(df)
   st.line_chart(df)
-  
+ 
+st.line_chart(dataframe)
  
