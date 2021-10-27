f396574cd14627f95569ffdc6afe1ad0b961d862
Eden_Lecarpentier <87129559+EdenLecarpentier@users.noreply.github.com>
2021-10-27 10:22:36 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -3,10 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px 
 
+pd.read_csv("nutrition_databse")
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None :
   df = pd.read_csv(uploaded_file, sep="|")
   st.write(df)
   st.line_chart(df)
- 
+    
